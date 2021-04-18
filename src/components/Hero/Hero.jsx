5b1073cee5c5a4d85a2015edc1e0c91d59c5f1ce
gxultimate <alquisadaralph@gmail.com>
2021-04-18 21:53:03 +0800

    april 18, 2020

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Container, Form } from 'react-bootstrap';
-import Fade from 'react-reveal/Fade';
+import { Container, Navbar } from 'react-bootstrap';
+import { Bounce, Fade } from 'react-reveal';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
 
-const Header = ({props ,setDark,isDark}) => {
-
+const Header = ({ props, setDark, isDark }) => {
   const { hero } = useContext(PortfolioContext);
- 
+
   const { title, subtitle, cta } = hero;
   const { name, job } = props.data;
 
@@ -23,36 +22,52 @@ const Header = ({props ,setDark,isDark}) => {
     }
   }, []);
 
-
-
   return (
-    <section id={isDark ? 'hero-dark' : 'hero'} className="jumbotron">
-      <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="100px">
-          <h1 className="hero-title">
-            {title || 'Hi, my name is '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
-            <br />
-            {job || "I'm the Unknown Developer."}
-            <img
-              onClick={setDark}
-              src={!isDark?"https://media.giphy.com/media/h8xE5ALopE7a9nGVIu/giphy.gif":"https://media.giphy.com/media/eGmgF0V90QIgpMv4WQ/giphy.gif"}
-              style={{ width: '7rem', height: '6rem', marginLeft: '2rem' }}
-              alt="loading..."
-            />
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="100px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
-              </Link>
-            </span>
-          </p>
-        </Fade>
-      </Container>
-    </section>
+    
+     
+  <section id={isDark ? 'hero-dark' : 'hero'} className="jumbotron">
+     
+   <Container>
+   <Navbar expand="lg" style={{justifyContent:"flex-end" ,position:"fixed" ,right:0 , top:0}} className={isDark ? 'hero-dark-navbar' : 'hero-navbar'}>
+      <Bounce right={isDesktop} bottom={isMobile} duration={1000} delay={600} distance="100px">
+          <img
+            onClick={setDark}
+            src={
+              !isDark
+                ? 'https://media.giphy.com/media/h8xE5ALopE7a9nGVIu/giphy.gif'
+                : 'https://media.giphy.com/media/eGmgF0V90QIgpMv4WQ/giphy.gif'
+            }
+            style={{ width: '4rem', height: '4rem' }}
+            alt="loading..."
+          />
+        </Bounce>
+  </Navbar>
+     <Bounce left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="100px">
+       <h1 className="hero-title">
+         {title || 'Hi, my name is '}
+         <span className="text-color-main">{name || 'Your Name'}</span>
+         <br />
+         {job || "I'm the Unknown Developer."}
+         <Bounce right={isDesktop} bottom={isMobile} duration={1000} delay={600} distance="100px">
+         <img src="https://media.giphy.com/media/UVG0BN8TOMKkPOJS6e/giphy.gif" style={{ width: '7rem', height: '7rem' , marginLeft:'2rem' }}/>
+       </Bounce>
+       </h1>
+       
+     </Bounce>
+     
+     <Bounce left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="100px">
+       <p className="hero-cta">
+         <span className="cta-btn cta-btn--hero">
+           <Link to="about" smooth duration={1000}>
+             {cta || 'Know more'}
+           </Link>
+         </span>
+       </p>
+     </Bounce>
+   </Container>
+ </section>
+   
+   
   );
 };
 
