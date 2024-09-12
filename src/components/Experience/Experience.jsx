de03931b9e9566d665fe1c6d0112c7f36a8d8911
gxultimate <43124533+gxultimate@users.noreply.github.com>
2024-09-13 03:18:24 +0800

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,1 +1,67 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import Tilt from 'react-tilt';
+import PortfolioContext from '../../context/context';
+import SkillImg from '../Image/SkillImg';
+import Title from '../Title/Title';
 
+const Experience = ({props ,isDark}) => {
+  const { experience } = useContext(PortfolioContext);
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const { experience } = props.data;
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id={isDark?"projects-dark":"projects"}>
+      <Container>
+        <div className="project-wrapper">
+          <Title title="Experience" />
+
+          <Row>
+            {experience.map((exp) => {
+              const { title, duration } = exp;
+
+              return (
+                <>
+                  <Col lg={3} sm={6}>
+                    <Fade
+                      left={isDesktop}
+                      bottom={isMobile}
+                      duration={1000}
+                      delay={500}
+                      distance="100px"
+                    >
+                      <div className="project-wrapper__text">
+                        <h3 className="project-wrapper__text-title">{title || 'Project Title'}</h3>
+                        <div>
+                          <p>
+                            {duration ||
+                              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                          </p>
+                        </div>
+                      </div>
+                    </Fade>
+                  </Col>
+                </>
+              );
+            })}
+          </Row>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Experience;
