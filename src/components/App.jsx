df1045a05e006e9254592e5447e70f7f60293a2f
gxultimate <43124533+gxultimate@users.noreply.github.com>
2024-09-13 03:21:04 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Projects from './Projects/Projects';
+import Experience from './Experience/Experience';
 import Skills from './Skills/Skills';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
 
 import { PortfolioProvider } from '../context/context';
 
-import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
+import { heroData, aboutData, projectsData, contactData, footerData, experienceData } from '../mock/data';
 
 function App(props) {
   const [isDark, setDark] = useState(false);
@@ -25,6 +26,7 @@ function App(props) {
   useEffect(() => {
     setHero({ ...heroData });
     setAbout({ ...aboutData });
+    setExperience({...experienceData});
     setProjects([...projectsData]);
     setContact({ ...contactData });
     setFooter({ ...footerData });
@@ -34,6 +36,7 @@ function App(props) {
     <PortfolioProvider value={{ hero, about, projects, contact, footer ,isDark}}>
       <Hero props={props} setDark={setDarkMode} isDark={isDark}/>
       <About props={props}  isDark={isDark} />
+       <Experience props={props }  isDark={isDark}/>
        <Skills props={props }  isDark={isDark}/>
       <Projects props={props}  isDark={isDark} />
       <Contact props={props}  isDark={isDark}/>
